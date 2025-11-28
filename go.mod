4462b11bdf3d1ee7f95c9e77e560416e69f9ce98
Francisco <francisco@MacBook-Pro-de-Francisco.local>
2025-11-28 13:48:37 -0300

    tipado en golang

---
+++ go.mod
@@ -0,0 +1,3 @@
+module curso_go/v2
+
+go 1.25.4
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	nombre := "Carlos"
+	edad := 30
+	altura := 1.82
+	activo := true
+	const PI = 3.14159
+	const NombreApp = "MiApp"
+
+	fmt.Println(nombre, edad, altura, activo, PI, NombreApp)
+}
